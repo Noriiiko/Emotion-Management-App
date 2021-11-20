3c45f6faceaa10ec14d2e4817a74412f68bc25f1
DarkDreamSher <291206667@qq.com>
2021-11-20 19:53:06 +0800

    record feelings UI

---
+++ app/src/main/java/com/example/emomanager/RecordFeelings.java
@@ -0,0 +1,42 @@
+package com.example.emomanager;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.emomanager.databinding.ActivityRecordFeelingsBinding;
+
+public class RecordFeelings extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityRecordFeelingsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityRecordFeelingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_record_feelings);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_record_feelings);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
